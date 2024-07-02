081ec4330f283591d859d6359713e7582599b099
EmanuJT <emanuelmartinez2395@gmail.com>
2024-07-01 18:36:30 -0600

    Add files via upload

---
+++ ACTIVIDAD__2/ESTRUCTURA_CONTROL/numero_mayor_menor_neutro.js
@@ -0,0 +1,9 @@
+let numero = -1;
+if (numero === 0){
+    console.log(numero + "El numero es neutro");
+}
+else if (numero > 0){
+    console.log(numero + "es un numero positivo")
+}else{
+    console.log(numero + "es un numero negativo")
+}
